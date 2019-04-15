f13c7c56534947e2be2558de3fe984f38c389e06
o5faruk <faruk.alibasic@gmail.com>
2019-04-15 14:15:18 -0700

    pagination and create

--- js/index.js
+++ js/index.js
@@ -1,11 +1,46 @@
-const jqxhr = $.get("api/cars", { skip: 0, limit: 20 }, function(data) {
-  reloadTable(data);
+$(function() {
+  getCars();
+
+  $("#createCarForm").on("submit", function(e) {
+    e && e.preventDefault();
+    var form = $(this);
+    $.ajax({
+      type: "POST",
+      url: "api/cars",
+      data: form.serialize(),
+      success: function() {
+        getCars();
+        $("#addCarModal").modal("toggle");
+        form.find("input[type=text], input[type=number]").val("");
+      }
+    });
+  });
 });
+
+const getCars = function() {
+  let limit = $("#pageSizeInput").val();
+  let page = $("#pageNumberInput").val();
+  let skip = limit * (page - 1);
+  console.log("RELOAD", skip, limit);
+  $.get("api/cars", { skip, limit }, function(data) {
+    reloadTable(data);
+  });
+};
 
 const reloadTable = function(cars) {
   var table = $("#cars_table");
   table.find("tbody tr").remove();
   cars.forEach(function(car) {
-    table.append(`<tr><td> ${car.id} </td><td> ${car.name} </td><td> ${car.model} </td><td> ${car.year} </td><td> ${car.power} </td><td></td></tr>`);
+    table.append(
+      `<tr><td> ${car.id} </td><td> ${car.name} </td><td> ${
+        car.model
+      } </td><td> ${car.year} </td><td> ${car.power} </td><td></td></tr>`
+    );
   });
+};
+
+const addPage = function(num) {
+  let page = $("#pageNumberInput").val();
+  $("#pageNumberInput").val(Number(page) + num);
+  getCars();
 };
